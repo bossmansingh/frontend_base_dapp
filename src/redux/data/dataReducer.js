52539123ce820c1d7db004a78e03a449670c6c73
Saurabh Singh <saurabh30590@gmail.com>
2021-09-08 14:15:03 -0700

    Fix issues

--- src/redux/data/dataReducer.js
+++ src/redux/data/dataReducer.js
@@ -1,10 +1,6 @@
 const initialState = {
   loading: false,
-<<<<<<< HEAD
   accountBalance: "",
-=======
-  name: "",
->>>>>>> 894d5c47a54bf99eb188d3f948996214c95dda97
   error: false,
   errorMsg: "",
 };
@@ -20,11 +16,7 @@ const dataReducer = (state = initialState, action) => {
       return {
         ...initialState,
         loading: false,
-<<<<<<< HEAD
         accountBalance: action.payload.accountBalance,
-=======
-        name: action.payload.name,
->>>>>>> 894d5c47a54bf99eb188d3f948996214c95dda97
       };
     case "CHECK_DATA_FAILED":
       return {
