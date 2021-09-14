e80a3cefd333112b8ebb0de822a1222067a8f838
Saurabh Singh <saurabh30590@gmail.com>
2021-09-13 23:38:12 -0700

    Added Flippy lib for welcome page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
 import { useDispatch, useSelector } from "react-redux";
 import { moralisAuthenticate, createGame } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
@@ -8,8 +9,8 @@ import * as s from "./styles/globalStyles";
 import Chessboard from "chessboardjsx";
 import logo from "./assets/chessboard_logo.jpg";
 
-
 function App() {
+  const ref = useRef();
   const dispatch = useDispatch();
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
@@ -48,7 +49,8 @@ function App() {
       fd={"row"}>
       <s.TextTitle
         ai={"center"}
-        style={{ color: "white", marginLeft: "auto", paddingLeft: (walletConnected ? "50px" : "110px") }}>
+        jc={"center"}
+        style={{ color: "white", marginLeft: "auto", paddingLeft: (walletConnected ? "5px" : "65px") }}>
         CHKMATE
       </s.TextTitle>
 
@@ -77,30 +79,45 @@ function App() {
     // TODO: Show minted NFTs (in carousel)
     return (
       <s.Container ai={"center"} jc={"center"} style={{paddingTop: "50px"}}>
-        <s.Container ai={"center"} jc={"center"} fd={"row"}>
-          <s.StyledButton style={{width:"130px", height:"40px"}}
-            onClick={(e) => {
-              e.preventDefault();
-              if (walletConnected && contractFetched) {
-                dispatch(createGame());
-              } else {
-                dispatch(moralisAuthenticate(true));
-              }
-              
-            }}>Create Game</s.StyledButton>
-          <s.SpacerMedium />
-          <s.StyledButton style={{width:"130px", height:"40px"}}
-            onClick={(e) => {
-              e.preventDefault();
-              // TODO: show how to play view
-            }}>How to play?</s.StyledButton>
-        </s.Container>
-        <s.SpacerMedium />
-        {blockchain.errorMsg !== "" ? (
-          <s.TextDescription>{blockchain.errorMsg}</s.TextDescription>
-        ) : null}
-        <s.SpacerMedium />
-        <Chessboard position="start" />
+        <Flippy 
+          flipDirection="horizontal"
+          ref={ref}
+        >
+          <FrontSide>
+            <s.Container ai={"center"} jc={"center"} fd={"row"}>
+              <s.StyledButton style={{width:"130px", height:"40px"}}
+                onClick={(e) => {
+                  e.preventDefault();
+                  if (walletConnected && contractFetched) {
+                    dispatch(createGame());
+                  } else {
+                    dispatch(moralisAuthenticate(true));
+                  }
+                  
+                }}>Create Game</s.StyledButton>
+              <s.SpacerMedium />
+              <s.StyledButton style={{width:"130px", height:"40px"}}
+                onClick={(e) => {
+                  e.preventDefault();
+                  ref.current.toggle();
+                }}>How to play?</s.StyledButton>
+            </s.Container>
+            <s.SpacerMedium />
+            {blockchain.errorMsg !== "" ? (
+              <s.TextDescription>{blockchain.errorMsg}</s.TextDescription>
+            ) : null}
+            <s.SpacerMedium />
+            <Chessboard position="start" />
+          </FrontSide>
+          <BackSide>
+            <s.Container ai={"center"}>
+              <s.TextDescription>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</s.TextDescription>
+              <s.StyledButton style={{marginTop:"50px"}} onClick={() => {
+                ref.current.toggle();
+              }}>Go back</s.StyledButton>
+            </s.Container>
+          </BackSide>
+        </Flippy>
       </s.Container>
     );
   }
