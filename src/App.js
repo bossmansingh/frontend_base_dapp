2e54be47851fa51fe7c7d0112067b09150ce1625
Saurabh Singh <saurabh30590@gmail.com>
2021-09-18 20:49:11 -0700

    Minor refactoring

--- src/App.js
+++ src/App.js
@@ -9,6 +9,31 @@ import * as s from "./styles/globalStyles";
 import Chessboard from "chessboardjsx";
 import logo from "./assets/chessboard_logo.jpg";
 
+const gameInstructions = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
+// The random color should be generated when the game starts, before 
+// that some default color should be used
+const lightSquareColor = getLightSquareColor();
+const darkSquareColor = getDarkSquareColor();
+
+// Function to generate and return light square color
+function getLightSquareColor() {
+  const min = 160;
+  const max = 255;
+  return `${getRandomNumber(min, max)},${getRandomNumber(min, max)},${getRandomNumber(min, max)}`;
+}
+
+// Function to generate and return dark square color
+function getDarkSquareColor() {
+  const min = 50;
+  const max = 140;
+  return `${getRandomNumber(min, max)},${getRandomNumber(min, max)},${getRandomNumber(min, max)}`;
+}
+
+// Function to generate random number 
+function getRandomNumber(min, max) { 
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 function App() {
   const ref = useRef();
   const dispatch = useDispatch();
@@ -18,10 +43,6 @@ function App() {
   const contract = blockchain.gameContract;
   const walletConnected = account !== null && account !== "";
   const contractFetched = contract != null;
-  // The random color should be generated when the game starts, before 
-  // that some default color should be used
-  const lightSquareColor = getLightSquareColor();
-  const darkSquareColor = getDarkSquareColor();
   console.log("lightSquareColor: ", lightSquareColor);
   console.log("darkSquareColor: ", darkSquareColor);
   console.table(blockchain);
@@ -48,25 +69,6 @@ function App() {
     </s.Screen>
   );
 
-  // Function to generate and return light square color
-  function getLightSquareColor() {
-    const min = 160
-    const max = 255
-    return `${getRandomNumber(min, max)},${getRandomNumber(min, max)},${getRandomNumber(min, max)}`;
-  }
-
-  // Function to generate and return dark square color
-  function getDarkSquareColor() {
-    const min = 50
-    const max = 140
-    return `${getRandomNumber(min, max)},${getRandomNumber(min, max)},${getRandomNumber(min, max)}`;
-  }
-
-  // Function to generate random number 
-  function getRandomNumber(min, max) { 
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
   function renderToolbar() {
     return <s.Container
       style={{ 
@@ -91,7 +93,9 @@ function App() {
           }}
         >
           { walletConnected && blockchain.identiconUrl != null ? (
-            <s.Identicon alt="identicon" src={(blockchain.identiconUrl != null ? blockchain.identiconUrl : logo)} />
+            <s.Identicon 
+              alt="identicon" 
+              src={(blockchain.identiconUrl != null ? blockchain.identiconUrl : logo)} />
           ) : (
             <s.StyledButton
               onClick={(e) => {
@@ -156,8 +160,9 @@ function App() {
                 Game instructions
               </s.TextTitle>
               <s.SpacerMedium />
-              <s.TextParagraph>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</s.TextParagraph>
-              <s.StyledButton style={{marginTop:"50px"}} onClick={() => {
+              <s.TextParagraph>{gameInstructions}</s.TextParagraph>
+              <s.SpacerMedium />
+              <s.StyledButton onClick={() => {
                 ref.current.toggle();
               }}>Go back</s.StyledButton>
             </s.Container>
